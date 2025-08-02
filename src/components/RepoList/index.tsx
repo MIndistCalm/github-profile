7a15fbbac345b43626b4e43f22afa45c9f95dcc6
MindistCalm <suxinin5@yandex.ru>
2025-08-02 23:52:06 +0700

    centered

--- src/components/RepoList/index.tsx
+++ src/components/RepoList/index.tsx
@@ -9,7 +9,7 @@ export const RepoList = ({ repos }: RepoListProps) => {
   if (!repos || repos.length === 0) return <div>No repositories found</div>
 
   return (
-    <div className="flex flex-row gap-4 flex-wrap items-stretch">
+    <div className="flex flex-row gap-4 flex-wrap items-stretch justify-center">
       {repos.map((repo, index) => (
         <RepoCard key={repo.id} repo={repo} idx={index} />
       ))}
